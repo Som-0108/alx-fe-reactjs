9480b2f8311cb6f19152c3d6fc227cbf767bd169
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-11-09 23:14:44 +0100

    Add files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,19 @@
 // src/App.jsx
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
+      {/* Display the welcome message */}
       <WelcomeMessage />
+
+      {/* Display the city components */}
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
