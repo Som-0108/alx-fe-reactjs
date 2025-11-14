4035e2a87004df9f7ceacbbab4ed55559ddbc543
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-11-15 00:39:55 +0100

    Add app

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,30 +1,14 @@
 // src/App.jsx
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile'; // ✅ import the new component
-import Counter from "./components/Counter"
 import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
+
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div>
-      {/* Display the welcome message */}
-      <WelcomeMessage />
-
-      {/* Display the city components */}
-      <Header />
-      <MainContent />
-      <Footer />
-
-      {/* ✅ Display the user profile */}
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
