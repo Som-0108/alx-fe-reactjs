369845364f20d43bb0116fee2b4cfd69b3e894f3
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-11-14 23:21:25 +0100

    Make changes

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,10 +1,17 @@
-// src/components/Footer.jsx
 function Footer() {
-    return (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
-    );
+  return (
+    <footer
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "20px",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
 }
 
 export default Footer;
