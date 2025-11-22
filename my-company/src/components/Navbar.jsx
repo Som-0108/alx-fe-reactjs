42e6464b20b93361451c0fbbe215c9a4e8e31d26
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-11-22 15:53:18 +0100

    Add my company

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+// src/components/Navbar.jsx
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "10px",
+        backgroundColor: "#f0f0f0"
+      }}
+    >
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
