4cd567fe9cd061000a9a591abca7f55686818165
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-11-22 15:55:50 +0100

    Addpackage.json

---
+++ my-company/src/App.jsx
@@ -0,0 +1,27 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+                 <Route path="/Navbar" element={<Navbar />} />
+
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
