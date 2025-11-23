9534786549f551f17f40d69d353d7c85102600ff
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-11-23 15:46:25 +0100

    Add app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+import RecipeDetails from './components/RecipeDetails'
+import FavoritesList from './components/FavoritesList'
+import RecommendationsList from './components/RecommendationsList'
 
-const App = () => {
+function App() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Recipe App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
-  );
-};
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
 
-export default App;
+        {/* ✅ Navigation */}
+        <nav>
+          <Link to="/">Home</Link> |{' '}
+          <Link to="/favorites">Favorites</Link> |{' '}
+          <Link to="/recommendations">Recommendations</Link>
+        </nav>
+
+        {/* ✅ Add new recipe form */}
+        <AddRecipeForm />
+
+        {/* ✅ Routes */}
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
